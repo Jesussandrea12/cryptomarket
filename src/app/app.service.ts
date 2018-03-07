07779a58266dcb5e3484fa5ed282df2d43db7d1b
jesus sandrea <jesussandrea12@gmail.com>
2018-03-07 10:58:57 -0500

    deploy to firebase

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ConfigService {
-  URL = 'https://api.coinmarketcap.com/v1/ticker/?limit=100';
+  URL = 'https://cors-anywhere.herokuapp.com/https://api.coinmarketcap.com/v1/ticker/?limit=100';
 
   constructor (private http: HttpClient) {}
 
